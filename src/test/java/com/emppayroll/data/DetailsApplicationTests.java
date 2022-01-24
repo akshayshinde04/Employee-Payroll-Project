5e7812b8ddd7b06872758c808f2e15442052015f
unknown <akshayHVshinde04@gmail.com>
2022-01-25 00:52:35 +0530

    EmployeePayroll

---
+++ src/test/java/com/emppayroll/data/DetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emppayroll.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
